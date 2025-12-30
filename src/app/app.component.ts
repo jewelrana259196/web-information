40e0351403fa1b661f8d16c558148bfd4c6adb10
jewelrana251196 <jewelrana251196@gmail.com>
2025-12-30 12:02:12 +0600

    Add home, education, experience components and update layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,18 @@ import { ProjectsComponent } from './projects/projects.component';
 import { SkillsComponent } from './skills/skills.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from "./home/home.component";
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent,
-    AboutComponent,
+  imports: [HomeComponent, HeaderComponent, AboutComponent,
     ProjectsComponent,
     SkillsComponent,
     ContactComponent,
-    FooterComponent],
+    FooterComponent, HomeComponent, ExperienceComponent, EducationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
